531a8f7514847c1b6c21b329c96cfa417853f6eb
Tarik Guney <DrTarik@Tariks-MBP-13.local>
2016-07-04 20:23:39 -0600

    Added Bubble Sort and Selection Sort

---
+++ BubbleSort/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace ConsoleApplication
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var numbers = new[] { 5, 4, 7, 3, 8, 1 };
+            for (int i = numbers.Length - 1; i > 0; i--)
+            {
+                for (int j = 0; j < i; j++)
+                {
+                    if (numbers[j] > numbers[j + 1])
+                    {
+                        var temp = numbers[j];
+                        numbers[j] = numbers[j + 1];
+                        numbers[j + 1] = temp;
+                    }
+                }
+            }
+            Console.WriteLine(string.Join(", ", numbers));
+        }
+    }
+}
---
+++ SelectionSort/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace ConsoleApplication
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var numbers = new[] { 1, 5, 7, 3, 8, 10, 34, 12 };
+            for (int outer = 0; outer < numbers.Length - 1; outer++)
+            {
+                var minNumberIndex = outer;
+                for (int inner = outer + 1; inner < numbers.Length; inner++)
+                {
+                    if (numbers[inner] < numbers[minNumberIndex])
+                    {
+                        minNumberIndex = inner;
+                    }
+                }
+
+                var temp = numbers[minNumberIndex];
+                numbers[minNumberIndex] = numbers[outer];
+                numbers[outer] = temp;
+            }
+
+            Console.WriteLine(string.Join(", ", numbers));
+        }
+    }
+}
