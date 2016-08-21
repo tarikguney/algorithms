a3c557c383f3105097cf155d9240b1843477b1f8
Tarik Guney <atarikguney@gmail.com>
2016-08-21 02:11:50 -0600

    Added more sample learning code for task parallel library

---
+++ task-async-code/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            try
+            {
+                Task t = Task.Run(() => { throw new Exception("Exception 1"); });
+                Task t2 = Task.Run(()=> {throw new Exception("Exception 2");});
+                t.Wait();
+            }
+            catch (Exception exception)
+            {
+                Console.WriteLine("Exception Message: " + exception.Message);
+                Console.WriteLine("Exception Type: " + exception.GetType().FullName);
+                Console.WriteLine("Exception Inner Message: " + exception.InnerException.Message);
+            }
+        }
+    }
+}
