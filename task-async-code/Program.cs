364b4d474f4af14d0c439bec4f79383440ef8d35
Tarik Guney <atarikguney@gmail.com>
2016-08-21 02:22:11 -0600

    Commited some changes

--- task-async-code/Program.cs
+++ task-async-code/Program.cs
@@ -9,9 +9,7 @@ namespace ConsoleApplication
         {
             try
             {
-                Task t = Task.Run(() => { throw new Exception("Exception 1"); });
-                Task t2 = Task.Run(()=> {throw new Exception("Exception 2");});
-                t.Wait();
+              PerformSomeTask().Wait();
             }
             catch (Exception exception)
             {
@@ -19,6 +17,13 @@ namespace ConsoleApplication
                 Console.WriteLine("Exception Type: " + exception.GetType().FullName);
                 Console.WriteLine("Exception Inner Message: " + exception.InnerException.Message);
             }
+        }
+
+        public static async Task PerformSomeTask()
+        {
+            Task t = Task.Run(() => { throw new Exception("Exception 1"); });
+            Task t2 = Task.Run(() => { throw new Exception("Exception 2"); });
+            await t;
         }
     }
 }
